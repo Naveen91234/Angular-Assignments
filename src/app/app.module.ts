50110c0ce0f39139381e202d4157f47f26dbcd02
S Naveen kumar <swayambarapu.kumar@kellton.com>
2024-05-08 14:50:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { AuthGuard } from './authGuard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contact/:contactId', component: ContactDetailsComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes) // Register the routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-details/contact-details.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-details',
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent {
+  constructor(private router: ActivatedRoute,) {}
+  ngOnInit() {
+    
+    this.router.paramMap.subscribe( paramMap => {
+      let c = paramMap.get('contactId');
+      console.log(c)
+  })
+  }
+
+}
